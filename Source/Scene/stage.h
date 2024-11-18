b5b5ba67588017cad0849a91761718d40f694c96
2230330 <2230330@MSI>
2024-11-18 16:08:16 +0900

    sine

--- Source/Scene/stage.h
+++ Source/Scene/stage.h
@@ -22,7 +22,7 @@ private:
 		DirectX::XMFLOAT3		velocity = { 0, 0, 0 };
 		DirectX::XMFLOAT3		position = { 0, 0, 0 };
 		DirectX::XMFLOAT3		angle = { 0, 0, 0 };
-		DirectX::XMFLOAT3		scale = { 1, 1, 1 };
+		DirectX::XMFLOAT3		scale = { 1., 1., 1. };
 		DirectX::XMFLOAT4X4		transform = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 		std::unique_ptr<Model>	model;
 
