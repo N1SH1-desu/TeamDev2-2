550f35d5e7d5b77026297f2f85f887d48f8cb6e5
N1SH1-desu <nishinn77@gmail.com>
2024-12-02 00:24:31 +0900

    当たり判定

---
+++ Source/Tetromino.cpp
@@ -0,0 +1,5 @@
+#include "Tetromino.h"
+
+namespace Tetromino
+{
+}
--- Source/Tetromino.h
+++ Source/Tetromino.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <type_traits>
+#include <array>
+
 namespace Tetromino
 {
-	enum class TetrominoType
+	enum TetrominoType
 	{
 		TETRO_T,
 		TETRO_S,
@@ -12,31 +15,110 @@ namespace Tetromino
 	};
 
 	template<TetrominoType T>
-	struct TetrominoBlock
+	struct TetrominoBlock : std::false_type
 	{
 	};
 
 	template <>
-	struct TetrominoBlock<TetrominoType::TETRO_T>
+	struct TetrominoBlock<TetrominoType::TETRO_T> : std::true_type
+	{
+		static constexpr std::array<std::array<uint8_t, 3>, 2> rotate0 = {
+			{
+			{ 1, 1, 1},
+			{ 0, 1, 0},
+			},
+		};
+
+		static constexpr std::array<std::array<uint8_t, 2>, 3> rotate1 = {
+			{
+				{0, 1},
+				{1, 1},
+				{0, 1},
+			},
+		};
+
+		static constexpr std::array<std::array<uint8_t, 3>, 2> rotate2 = {
+			{
+				{0, 1, 0},
+				{1, 1, 1},
+			}
+		};
+
+		static constexpr std::array<std::array<uint8_t, 2>, 3> rotate3 = {
+			{
+				{1, 0},
+				{1, 1},
+				{1, 0},
+			}
+		};
+	};
+
+	template <>
+	struct TetrominoBlock<TetrominoType::TETRO_S> : std::true_type
 	{
 	};
 
 	template <>
-	struct TetrominoBlock<TetrominoType::TETRO_S>
+	struct TetrominoBlock<TetrominoType::TETRO_Z> : std::true_type
 	{
 	};
 
-	template <>
-	struct TetrominoBlock<TetrominoType::TETRO_Z>
-	{
-	};
+#define Rotate(type, rotateIndex) ( TetrominoBlock<type>::rotate ## rotateIndex )
 
 	class TetrominoCollider
 	{
 	public:
 		TetrominoCollider() = default;
 
+		template<TetrominoType Type>
+		void PlaceTetromino(unsigned int top, unsigned int left, int rotate)
+		{
+			static_assert(TetrominoType<Type>::value == true, "This type is invalid");
 
+			if (!DetectionCollide<Type>(x, y, rotate))
+			{
+				return;
+			}
+
+			auto shapeArray = Rotate(x, y, rotate);
+
+			for (int row = 0; row < shapeArray.size(); ++row)
+			{
+				for (int col = 0; col < shapeArray[row].size(); ++col)
+				{
+					if (shapeArray[row][col] != 0)
+					{
+						Placed[row + top][col + left] = shapeArray[row][col];
+					}
+				}
+			}
+
+
+		}
+
+		template<TetrominoType Type>
+		bool DetectionCollide(unsigned int top, unsigned int left, unsigned int rotate)
+		{
+			static_assert(TetrominoType<Type>::value == true, "This type is invalid");
+
+			auto shapeArray = Rotate(Type, rotate);
+
+			for (int row = 0; row < shapeArray.size(); ++row)
+			{
+				for (int col = 0; col < shapeArray[row].size(); ++col)
+				{
+					if (shapeArray[row][col] != 0)
+					{
+						if (Placed[row + top][col + left] != 0)
+						{
+							return false;
+						}
+					}
+				}
+			}
+
+			return true;
+		}
 
 	private:
 		static constexpr unsigned int ROW_LENGTH = 9u;
