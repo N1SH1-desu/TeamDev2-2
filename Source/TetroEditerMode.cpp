b99de07fbd77625a2b77e5b8be54a8cc5cf7c275
N1SH1-desu <nishinn77@gmail.com>
2024-12-09 16:23:38 +0900

    cpp

---
+++ Source/TetroEditerMode.cpp
@@ -0,0 +1,13 @@
+#include "TetroEditerMode.h"
+
+void TetroEditerMode::Initialize(ID3D11Device* device, ID2D1DeviceContext* d2dContext)
+{
+}
+
+void TetroEditerMode::Update(float elapsedTime, POINTS mousePos, const Input::KeyInput& keyInput)
+{
+}
+
+void TetroEditerMode::Render(RenderContext rc, ID2D1DeviceContext* d2dContext, ModelRenderer* mRenderer)
+{
+}
