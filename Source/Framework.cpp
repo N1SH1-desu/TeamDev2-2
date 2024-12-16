20672ca1b2cf82da2e7597b1acbc8aa2d140a417
N1SH1-desu <nishinn77@gmail.com>
2024-12-16 15:55:11 +0900

    a

--- Source/Framework.cpp
+++ Source/Framework.cpp
@@ -174,7 +174,7 @@ void Framework::CalculateFrameStats()
 		std::ostringstream outs;
 		outs.precision(6);
 		outs << "FPS : " << fps << " / " << "Frame Time : " << mspf << " (ms)";
-		SetWindowTextA(hWnd, outs.str().c_str());
+		//SetWindowTextA(hWnd, outs.str().c_str());
 
 		// Reset for next average.
 		frames = 0;
